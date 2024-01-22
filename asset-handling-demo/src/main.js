f85fcbdf5992edf6b331413d363e69c201e30ece
Sean Stocker <sean.stocker14@gmail.com>
2024-01-22 22:09:57 +0000

    New Demo feat: added asset handling demo

---
+++ asset-handling-demo/src/main.js
@@ -0,0 +1,5 @@
+import image from "./image.jpg";
+
+const imgElement = document.createElement("img");
+imgElement.src = image;
+document.body.appendChild(imgElement);
---
+++ asset-handling-demo/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/main.js",
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
